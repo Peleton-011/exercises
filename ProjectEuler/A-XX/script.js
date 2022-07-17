3976a6d138982ee8d1eeeecd895de7fb107745ab
Nico <nicodiazan@gmail.com>
2022-07-17 15:58:58 +0200

    Reformat code and add comments for rreadability

--- ProjectEuler/A-XX/script.js
+++ ProjectEuler/A-XX/script.js
@@ -1,13 +1,14 @@
 const r = document.querySelector(":root");
 
+//Sets everything up, all one time procedures are done here
 function setup() {
     setButtonWidth(".left-buttons");
     setButtonWidth(".right-buttons");
 }
 
+//Sets width of all contiguous elements to that of the widest element
 function setButtonWidth(selector) {
     let buttons = document.querySelector(selector).children;
-    console.log(buttons);
     let maxWidth = 0;
 
     // Find widest button
@@ -16,45 +17,44 @@ function setButtonWidth(selector) {
         maxWidth = Math.max(maxWidth, btn.offsetWidth);
     }
 
-    // let padding = pxToNum(getComputedStyle(r).getPropertyValue("--button-padding"));
-    // maxWidth += 2 * padding;
-
+    //Extra space as padding just in case
     maxWidth += 6;
-    
-    // Set width of all buttons to the max
+
+    // Set width of all buttons to the max one
     for (let i = 0; i < buttons.length; i++) {
         let btn = buttons[i];
-        btn.style.width = (maxWidth) + "px";
+        btn.style.width = maxWidth + "px";
     }
 }
 
 //Returns the next exercise
-function findNext() {
+function findNextPage() {
     let docName = window.location.pathname;
     let splitName = docName.split("/");
 
     //Get folder name to find the next one
-    docName = splitName[splitName.length -2];
-    
+    docName = splitName[splitName.length - 2];
+
+    //Split name into tag and num
+    //A-32 -> A is the tag, 32 is the num
     let tag = docName.split("-")[0];
     let num = Number(docName.split("-")[1]);
-    console.log(num)
-    //Set the number to the next one 
-    num += 1;
-    console.log(num);
 
-    //Check if it goes over 100
+    //Set the number to the next one
+    num += 1;
+
+    //Check if it goes over 100, if it does go to next tag
     if (Math.floor(num / 100)) {
         tag = nextLetter(tag);
     }
-    num %= 100
-    console.log(num);
+    num %= 100;
 
+    //Put updated name back together
     num = String(num);
     num = num.padStart(2, "0");
 
     docName = tag + "-" + num;
-    
+
     return docName;
 }
 
@@ -71,12 +71,4 @@ function nextLetter(str) {
     });
 }
 
-// function pxToNum (px) {
-//     px = px.replace(" ", "");
-//     px = px.replace("px", "");
-//     return Number(px);
-// }
-
 setup();
-
-console.log(findNext());
