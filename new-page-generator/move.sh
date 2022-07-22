474e4a48326aae6034d8ae59bf9be6859070ba81
Nico <nicodiazan@gmail.com>
2022-07-22 17:24:16 +0200

    Add tag and number calculation to dynamically set filename and path

--- new-page-generator/move.sh
+++ new-page-generator/move.sh
@@ -1,14 +1,17 @@
 for i in *{0..9}.html ; do
-    [ -f "$i" ] || break
+    [ -f "$i" ] || continue
     i=${i:8}
     i=${i%".html"}
     LEN=${#i}
+    HUNDRED=$[$[i-$[i%100]]/100]
     if [ $LEN -gt 2 ] ; then
-        echo "test1"
+        i=${i:$[$LEN-2]}
     elif [ $LEN -lt 2 ] ; then
         i="0$i"
     fi
-    PATH="../ProjectEuler/A-XX/"
+    printf -v TAG "\x$(printf %x $((${HUNDRED}+65)))"
+    PATH="../ProjectEuler/${TAG}-${i}/"
+    echo ${PATH}
     if [ -d $PATH ] ; then
         echo "test2"
     fi
