2775a9ccf0590323b1851ba32f10f20b34f42fa2
Nico <nicodiazan@gmail.com>
2022-07-22 12:57:13 +0200

    Fix move.sh, the problem was the elif command

--- new-page-generator/move.sh
+++ new-page-generator/move.sh
@@ -2,15 +2,14 @@ for i in *{0..9}.html ; do
     [ -f "$i" ] || break
     i=${i:8}
     i=${i%".html"}
-    if [ "${#i}" > "2" ] ; then
+    LEN=${#i}
+    if [ $LEN -gt 2 ] ; then
         echo "test1"
-    else if [ "${#i}" < "2" ] ; then
-        i=0$i
+    elif [ $LEN -lt 2 ] ; then
+        i="0$i"
     fi
-    PATH="../ProjectEuler"
-    if [ -d "$PATH" ] ; then
+    PATH="../ProjectEuler/A-XX/"
+    if [ -d $PATH ] ; then
         echo "test2"
     fi
-    echo $i
-    echo ${#i}
 done
