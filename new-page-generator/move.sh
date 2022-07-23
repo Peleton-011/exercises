9918fa46a10a290d52773ed91d2251a2e3c51605
Nico <nicodiazan@gmail.com>
2022-07-23 10:37:27 +0200

    Add copying scraped contents to designated page file

--- new-page-generator/move.sh
+++ new-page-generator/move.sh
@@ -2,9 +2,12 @@
 for i in *{0..9}.html ; do
     #Do for all existing Problem-XX.html files
     [ -f "$i" ] || continue
+    #Get file content
+    CONTENT=$(<"$i")
     #Get problem number
     i=${i:8}
     i=${i%".html"}
+    ONUM=$i
     LEN=${#i}
     #Set correct number
     HUNDRED=$(($((i-$((i%100))))/100))
@@ -15,10 +18,14 @@ for i in *{0..9}.html ; do
     fi
     #Set tag
     printf -v TAG "\x$(printf %x $((HUNDRED+65)))"
+    NAME="${TAG}-${i}"
     #Set path
-    PROBLEM="../ProjectEuler/${TAG}-${i}"
+    PROBLEM="../ProjectEuler/${NAME}"
     #Copy dir and edit insides
     cp -r ../ProjectEuler/A-XX "${PROBLEM}"
     cd "${PROBLEM}" || exit
-    mv "./A-XX.html" "./${TAG}-${i}.html"
+    mv "./A-XX.html" "./${NAME}.html"
+    sed "s/Exercice/Exercice ${ONUM}/" "./${NAME}.html"
+    #Add content to insides
+    sed "s/Sample problem text/${CONTENT}/" "../ProjectEuler/${NAME}/${NAME}.html"
 done
