1f9b71f102c407770fff39555b7b60683525c5c1
Nico <nicodiazan@gmail.com>
2022-07-23 10:26:03 +0200

    Add folder and html generator for new problem pages

--- new-page-generator/move.sh
+++ new-page-generator/move.sh
@@ -7,15 +7,18 @@ for i in *{0..9}.html ; do
     i=${i%".html"}
     LEN=${#i}
     #Set correct number
-    HUNDRED=$[$[i-$[i%100]]/100]
+    HUNDRED=$(($((i-$((i%100))))/100))
     if [ "$LEN" -gt 2 ] ; then
-        i=${i:$[$LEN-2]}
+        i=${i:$((LEN-2))}
     elif [ "$LEN" -lt 2 ] ; then
         i="0$i"
     fi
     #Set tag
-    printf -v TAG "\x$(printf %x $((${HUNDRED}+65)))"
-    #Set path and copy template to path
+    printf -v TAG "\x$(printf %x $((HUNDRED+65)))"
+    #Set path
     PROBLEM="../ProjectEuler/${TAG}-${i}"
+    #Copy dir and edit insides
     cp -r ../ProjectEuler/A-XX "${PROBLEM}"
+    cd "${PROBLEM}" || exit
+    mv "./A-XX.html" "./${TAG}-${i}.html"
 done
