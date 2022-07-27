695bae25ba3cdea4b476bbc0fbf43e1f41654b5e
Nico <nicodiazan@gmail.com>
2022-07-27 17:06:22 +0200

    Add solution to problem 1

--- ProjectEuler/A-01/solution.js
+++ ProjectEuler/A-01/solution.js
@@ -1,6 +1,12 @@
-let result;
+let result = 0;
 
-//Code ...
+const targetNum = 1000;
 
-const resultDiv = document.querySelector(".sample-selector");
-resultDiv.textContent = result;
+for (let i = 0; i < targetNum; i++) {
+    if ((i % 3 == 0) || (i % 5 == 0)) {
+        result += i;        
+    };
+}
+
+const resultDiv = document.querySelector(".solution");
+resultDiv.textContent = resultDiv.textContent + String(result);
