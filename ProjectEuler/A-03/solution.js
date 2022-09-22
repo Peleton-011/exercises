daf584f23291ce96e30558ff2f84a85254245ca2
Nico <nicodiazan@gmail.com>
2022-09-22 22:43:51 +0200

    Complete Style-1

--- ProjectEuler/A-03/solution.js
+++ ProjectEuler/A-03/solution.js
@@ -1,6 +1,32 @@
 let result;
 
-//Code ...
+let targetNum = 13195;
 
-const resultDiv = document.querySelector(".sample-selector");
-resultDiv.textContent = result;
+let currNum = 2;
+let primes = [2];
+
+checknum:
+while (currNum < targetNum) {
+    console.log(currNum);
+    //Check if it's prime
+    if (primes.every((prime) => {
+        console.log(`${currNum} % ${prime} = ${currNum % prime}`)
+        return (currNum % prime == 0);
+        })) {
+        console.log("CUM")
+        //Increase currNum and go to next number if not prime
+        currNum++;
+        continue checknum;
+    }
+    console.log(currNum)
+    //Add prime to list, divide target, and
+    targetNum /= currNum;
+    primes.push(currNum);
+    currNum++;
+}
+console.log(primes);
+
+result = primes[primes.length - 1];
+
+const resultDiv = document.querySelector(".solution");
+resultDiv.textContent = resultDiv.textContent + result;
